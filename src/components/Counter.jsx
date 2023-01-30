55f099b71f47bbe7e567be9084d89741bb3888d1
Dino Lindlau <dino@lindlau.dev>
2023-01-30 23:11:57 +0100

    Update prettier plugins

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,11 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Counter() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    console.log("count changed: ", count)
+  }, [count])
+
   return <button onClick={() => setCount(count + 1)}>count: {count}</button>
 }
