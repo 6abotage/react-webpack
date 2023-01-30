f120349c7731c6609f7d190b60a556cd1ebd5a2a
Dino Lindlau <dino@lindlau.dev>
2023-01-30 22:43:57 +0100

    Create Counter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from "react"
+import Counter from "./components/Counter"
 
 export default function App() {
-  const [count, setCount] = useState(0)
   return (
     <div>
       <h1>My App</h1>
-      <button onClick={() => setCount(count + 1)}>{count} </button>
+      <Counter />
     </div>
   )
 }
